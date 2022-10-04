4b1a25cb9db890579e2377b0e9ef9ecdbe27aa5b
Allen Gregory <allengregoryiv@gmail.com>
2022-10-03 23:23:47 -0500

    Update CommandExamples.java

--- src/main/java/frc/robot/commands/CommandExamples.java
+++ src/main/java/frc/robot/commands/CommandExamples.java
@@ -2,16 +2,29 @@ package frc.robot.commands;
 
 public class CommandExamples {
     //Examples: https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-    //new ConditionalCommand(commandOnTrue, commandOnFalse, m_limitSwitch::get)
-    //new InstantCommand(m_hatchSubsystem::grabHatch, m_hatchSubsystem)
     //new RunCommand(()-> Robot.climber.goToPosition(position), Robot.climber);
+    //new InstantCommand(m_hatchSubsystem::grabHatch, m_hatchSubsystem)
     //new StartEndCommand(() -> m_shooter.shooterSpeed(0.5), () -> m_shooter.shooterSpeed(0.0), m_shooter))
-    //FunctionalCommand
+    //new ConditionalCommand(commandOnTrue, commandOnFalse, m_limitSwitch::get)
     //new PrintCommand("This message will be printed!")
     //new WaitCommand(5)
     //new WaitUntilCommand(m_limitSwitch::get)
     //new PerpetualCommand(commandToRunForever)
     //new ScheduleCommand(commandToSchedule) , only used to fork off a command group
+
+    //FunctionalCommand
+/*     new FunctionalCommand(
+        // onInit Reset encoders on command start
+        m_robotDrive::resetEncoders,
+        // onExecute Start driving forward at the start of the command
+        () -> m_robotDrive.arcadeDrive(kAutoDriveSpeed, 0),
+        // onEnd Stop driving at the end of the command
+        interrupted -> m_robotDrive.arcadeDrive(0, 0),
+        // isFinished End the command when the robot's driven distance exceeds the desired value
+        () -> m_robotDrive.getAverageEncoderDistance() >= kAutoDriveDistanceInches,
+        // requirements Require the drive subsystem
+        m_robotDrive
+    ) */
 
     //Decorators
     //.withTimeout(5)
@@ -19,7 +32,7 @@ public class CommandExamples {
     //.andThen() adds a method to run after a command ends
     //.beforeStarting adds a method to be executed before the command starts:
     //.alongWith adds a method to be executed in a parrellel command group with the command
-    //.raceWith makes a parrellel race group that ends when the first command ends
+    //.raceWith makes a parrellel race group that ends when any of the commands end
     //.deadlineWith makes a parrellel deadline group and when the first method ends all the methods end
     //.withName adds a name to a command
     //.perpetually removes the end condition
